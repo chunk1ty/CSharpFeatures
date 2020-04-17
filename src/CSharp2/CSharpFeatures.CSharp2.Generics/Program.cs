f18d562fe3d0b947e42b7cf3102e2dd6cc756adb
chunk1ty <andriyan.krastev@gmail.com>
2020-04-17 16:49:31 +0300

    initial commit

---
+++ src/CSharp2/CSharpFeatures.CSharp2.Generics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpFeatures.CSharp2.Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
